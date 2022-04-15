d81b1f1daeacc1b62e4b5624df9b45efe5bd4524
madeinfree <sal95610@gmail.com>
2022-04-15 08:56:42 +0800

    test: add gas-report plugin
    * add gas-report plugin to know test recover and gasfee

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  gasReporter: {
+    enabled: true,
+  },
 };
