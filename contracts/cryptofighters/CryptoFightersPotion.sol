e51e42101f07eb3f92ebb84159ac6673e9c60f5c
davidberiro <deebenchimol@gmail.com>
2022-04-26 14:23:05 +0300

    update

--- contracts/cryptofighters/CryptoFightersPotion.sol
+++ contracts/cryptofighters/CryptoFightersPotion.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
